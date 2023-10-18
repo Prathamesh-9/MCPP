5b18b07e9c5bb485b085d105da022d782219128d
Prathamesh-9 <prathamesh.pashte@fractal.ai>
2023-10-18 16:45:56 +0000

    first_commit_new

--- hvt1.py
+++ hvt1.py
@@ -1,18 +1,13 @@
 import pandas as pd
 
-customers = pd.read_csv('customers.csv')
-orders = pd.read_csv('orders.csv')
-transactions = pd.read_csv('transactions.csv')
+customers = pd.read_csv(r'C:\Users\Administrator\Desktop\MC GIT\MCPP\customers.csv')
+orders = pd.read_csv(r'C:\Users\Administrator\Desktop\MC GIT\MCPP\orders.csv')
+transactions = pd.read_csv(r'C:\Users\Administrator\Desktop\MC GIT\MCPP\transactions.csv')
 
 merged_data = pd.merge(orders, transactions, on='order_id')
 merged_data = pd.merge(merged_data, customers, on='customer_id')
 
-hvt = merged_data[merged_data['sales']>250]
+hvt = merged_data[merged_data['sales']>150]
+hvt_by_oc = hvt.groupby(by=['order_id','customer_name'])
+print(hvt_by_oc[['order_id','customer_name']].head())
 
-from datetime import datetime as dt
-hvt['order_month'] = pd.to_datetime(hvt['order_purchase_date'].str.strip()).dt.month
-hvt['order_year'] = pd.to_datetime(hvt['order_purchase_date'].str.strip()).dt.year
-
-hvt_new = hvt.groupby(by=['order_month','order_year'])['order_id'].size()
-
-print(hvt_new)
